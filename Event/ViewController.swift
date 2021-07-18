c724ad8e8cc285881f166b4396f18019de3f7f76
João <joaolds@iMac-de-Joao.local>
2021-07-18 12:25:26 -0300

    Criacao do projeto

---
+++ Event/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Event
+//
+//  Created by João Luis dos Santos on 18/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
